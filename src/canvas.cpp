e6c531777e42523e776d1c1d3b4beacf77c33521
Joel <jtorres@haverford.edu>
2023-02-23 14:56:49 -0500

    better than before but no drawings

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -36,17 +36,20 @@ void Canvas::begin(PrimitiveType type)
    else if(type==UNDEFINED){
       dada.clear();
    }
+   dada.clear();
 }
 
 void Canvas::end()
 {
-   if(check=2 && sizeof(dada)%2==0){//%2= modulo 2
-      for(int i=0;i<sizeof(dada);i+=2){
+   printf("dada size: %d\n", dada.size());
+   if(check=2 && dada.size()%2==0){//%2= modulo 2
+      for(int i=0;i<dada.size();i+=2){
          line(dada[i],dada[i+1]);
       }
    }
-   else if(check=3 && sizeof(dada)%3==0){
-      for(int i=0;i<sizeof(dada);i+=3){
+   else if(check=3 && dada.size()%3==0){
+      printf("here\n");
+      for(int i=0;i<dada.size();i+=3){
          triangle(dada[i],dada[i+1],dada[i+2]);
       }
    }
@@ -78,8 +81,8 @@ void Canvas::background(unsigned char r, unsigned char g, unsigned char b)
    bround.b=b;
    _canvas.fill(bround);//fills the canvas with specified color/makes background
 }
-
 void Canvas::line(dad p1,dad p2){
+   printf("drawing line\n");
    int w=p2.x-p1.x;
    int h=p2.y-p1.y;
    if(p1.y>p2.y){
@@ -95,8 +98,8 @@ void Canvas::line(dad p1,dad p2){
       for(int y=p1.y;y<=p2.y;y++){
          float cr=sqrt(pow(dada[0].x-x,2)+pow(dada[0].y-y,2))/sqrt(pow(dada[1].x-x,2)+pow(dada[1].y-dada[0].y,2));
          tmp.r=(dada[0].rgb.r*(1-cr))+(dada[1].rgb.r*cr);
-         tmp.r=(dada[0].rgb.g*(1-cr))+(dada[1].rgb.g*cr);
-         tmp.r=(dada[0].rgb.b*(1-cr))+(dada[1].rgb.b*cr);
+         tmp.g=(dada[0].rgb.g*(1-cr))+(dada[1].rgb.g*cr);
+         tmp.b=(dada[0].rgb.b*(1-cr))+(dada[1].rgb.b*cr);
          _canvas.set(x,y,tmp);
          if(f>0){
             x+=dx;
@@ -142,10 +145,11 @@ float Canvas::mth(dad v1,dad v2,dad v3){//see week 4/5 slides
 }
 
 void Canvas::triangle(dad p1, dad p2, dad p3){
-   int mx=max(p1.x,p2.x,p3.x);
-   int my=max(p1.y,p2.y,p3.y);
-   int mnx=min(p1.x,p2.x,p3.x);
-   int mny=min(p1.y,p2.y,p3.y);
+   printf("drawing triangle\n");
+   int mx=max(max(p1.x,p2.x),p3.x);
+   int my=max(max(p1.y,p2.y),p3.y);
+   int mnx=min(min(p1.x,p2.x),p3.x);
+   int mny=min(min(p1.y,p2.y),p3.y);
    for(int y=mny;y<my;y++){
       for(int x=mnx;x<mx;x++){
          buff.x=x;
@@ -156,6 +160,7 @@ void Canvas::triangle(dad p1, dad p2, dad p3){
          alpha=mth(buff,p2,p3)/mth(p1,p2,p3);
          beta=mth(buff,p3,p1)/mth(p2,p3,p1);
          gamma=mth(buff,p1,p2)/mth(p3,p1,p2);
+         _canvas.set(y,x,buff.rgb);
       }
    }
 }
